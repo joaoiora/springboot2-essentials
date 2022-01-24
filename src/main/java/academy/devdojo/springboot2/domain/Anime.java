8a8c8cbca9dc49a5410ddc26d3c11b2923a17224
João Iora <joaoiora.42@gmail.com>
2022-01-24 19:33:14 -0300

    initial Spring Data & Hibernate configuration

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,14 +1,22 @@
 package academy.devdojo.springboot2.domain;
 
+
 import lombok.*;
+import org.springframework.stereotype.*;
+
+import javax.persistence.*;
 
 /**
  * @author Joao Iora
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
   private String name;
