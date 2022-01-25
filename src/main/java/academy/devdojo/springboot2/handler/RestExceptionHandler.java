2360e1e34d6442e0604f07172c2760ef5ebf80fc
João Iora <joaoiora.42@gmail.com>
2022-01-25 09:42:09 -0300

    Exception Handler

--- src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package academy.devdojo.springboot2.handler;
 
 import academy.devdojo.springboot2.exception.*;
+import lombok.extern.log4j.*;
 import org.springframework.http.*;
+import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.*;
@@ -10,11 +12,24 @@ import java.time.*;
  * @author Joao Iora
  */
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
   
   @ExceptionHandler(value = BadRequestException.class)
   public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException exception) {
     return new ResponseEntity<>(BadRequestExceptionDetails.builder()
+                                                          .title("Bad Request Exception. Check the API documentation.")
+                                                          .timestamp(LocalDateTime.now())
+                                                          .status(HttpStatus.BAD_REQUEST.value())
+                                                          .details(exception.getMessage())
+                                                          .developerMessage(exception.getClass().getName())
+                                                          .build(), HttpStatus.BAD_REQUEST);
+  }
+  
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  public ResponseEntity<ValidationExceptionDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    log.info("Fields {}", exception.getBindingResult().getFieldError().getField());
+    return new ResponseEntity<>(ValidationExceptionDetails.builder()
                                                           .title("Bad Request Exception. Check the API documentation.")
                                                           .timestamp(LocalDateTime.now())
                                                           .status(HttpStatus.BAD_REQUEST.value())
